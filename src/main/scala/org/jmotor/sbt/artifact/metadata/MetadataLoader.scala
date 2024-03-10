143b29b3e5a48837e255807ba0f4061e7501208b
Roman Langolf <rolang@pm.me>
2024-03-10 12:56:01 +0700

    use output stream on download

--- src/main/scala/org/jmotor/sbt/artifact/metadata/MetadataLoader.scala
+++ src/main/scala/org/jmotor/sbt/artifact/metadata/MetadataLoader.scala
@@ -5,12 +5,13 @@ import org.apache.ivy.util.{CopyProgressEvent, CopyProgressListener}
 import org.apache.maven.artifact.versioning.ArtifactVersion
 import org.jmotor.sbt.artifact.exception.ArtifactNotFoundException
 
-import java.net.URL
+import java.net.{URL, URI}
 import java.nio.file.{Files, Path, Paths}
 import scala.concurrent.{ExecutionContext, Future, Promise}
 import scala.io.Source
 import java.io.ByteArrayInputStream
 import java.net.HttpURLConnection
+import java.io.FileOutputStream
 
 /**
  * Component: Description: Date: 2018/2/8
@@ -29,14 +30,25 @@ trait MetadataLoader {
   def download(organization: String, artifactId: String, url: String)(implicit ec: ExecutionContext): Future[Path] =
     Future {
       try {
-        val src        = new URL(url)
+        val src        = new URI(url).toURL()
         val connection = src.openConnection()
 
         Option(connection.getInputStream()).map { is =>
           val path = Files.createTempFile(s"maven-metadata-$organization-$artifactId", ".xml")
-          // might better write via file output stream
-          Files.write(path, is.readAllBytes())
-          path
+          val os   = new FileOutputStream(path.toAbsolutePath.toString())
+
+          try {
+            val buffer    = new Array[Byte](8192)
+            var bytesRead = is.read(buffer)
+            while (bytesRead >= 0) {
+              os.write(buffer, 0, bytesRead)
+              bytesRead = is.read(buffer)
+            }
+            path
+          } finally {
+            is.close()
+            os.close()
+          }
         }
       } catch {
         case e: java.io.FileNotFoundException => None
